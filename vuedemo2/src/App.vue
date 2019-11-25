1c5c69e76c2c5b749b372b0e5d646dfbfe08cc80
heyaonan <1049386140@qq.com>
2019-11-25 18:33:26 +0800

    深入理解组件

--- vuedemo2/src/App.vue
+++ vuedemo2/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <Login><span slot="header">Login</span></Login>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import Login from './components/ComLogin'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Login
   }
 }
 </script>
