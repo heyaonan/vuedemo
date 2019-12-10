6ee849d319fa65262e40eb3c1ba01ee24cb4f2e1
heyaonan <1049386140@qq.com>
2019-12-10 20:52:36 +0800

    开发首页

--- travel/src/main.js
+++ travel/src/main.js
@@ -1,8 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
+import '@/assets/styles/reset.css'
+import '@/assets/styles/border.css'
+import '@/assets/styles/iconfont.css'
 
 Vue.config.productionTip = false
 
